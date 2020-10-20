f0f20d785ffa7af48d96484118d1359291073d9e
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-10-20 12:21:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+<<<<<<< HEAD
     get '/name' do
     "My name is Nathaniel"
     end
@@ -13,4 +14,14 @@ class App < Sinatra::Base
     "My favorite song is Down Under"
     end
 
+=======
+  get '/' do
+  "Hello, World!"
+  
+  get '/' do
+  "Hello, World!"
+  
+  get '/' do
+  "Hello, World!"
+>>>>>>> d6697f275e1a7606b5d81c8e4cec2bd16f017594
 end
